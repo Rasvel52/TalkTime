4c316073745f50e684e2297e740bb6d80d1f50a5
Rasvel52 <kostia62975@gmail.com>
2024-05-26 22:54:10 +0300

    update

---
+++ README.md
@@ -0,0 +1,13 @@
+# Components:
+```
+pip install flask
+pip install flask_sqlalchemy
+```
+# Server Start:
+```
+python application.py
+```
+# SQL DATA BROWSER:
+```
+https://sqlitebrowser.org
+```
---
+++ application.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+db = SQLAlchemy(app)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        return f'<Post {self.id}>'
+app.app_context().push()
+db.create_all()
+
+posts = []
+
+@app.route('/')
+def main():
+    posts = Post.query.all()
+    return render_template('index.html', posts=posts)
+
+@app.route('/')
+def re_main():
+    return redirect(url_for('main'))
+
+@app.route('/add_post', methods=['GET', 'POST'])
+def add_post():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        new_post = Post(title=title, content=content)
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect(url_for('main'))
+    else:
+        return redirect(url_for('main'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
